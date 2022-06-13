f62cff5b397f19be31e02183b2ec869b0ef157a9
Richard Abhulimhen <richardabhu@fb.com>
2022-06-13 16:31:31 -0700

    "Made splash screen display for 1 second" "Added Login UI"

--- app/src/main/java/com/example/instagram/SplashActivity.java
+++ app/src/main/java/com/example/instagram/SplashActivity.java
@@ -1,15 +1,34 @@
 package com.example.instagram;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+
+    public static final String TAG = "SplashActivity";
+    public static final int MILLISECONDS_DELAY = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // hide action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        // delayed loading
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, MILLISECONDS_DELAY);
     }
 }
